17226a9ffe60a66a04d2be0acabad5ba94c94309
Atanas Kovachki <crashdeburn@gmail.com>
2019-09-29 11:59:01 +0200

    Add files via upload

---
+++ engine/bullet_energy/library/language/utf-8/English/datepicker_local.js
@@ -0,0 +1,22 @@
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+jQuery(function($){
+	$.datepicker.regional['en'] = {
+		closeText: 'Close',
+		prevText: '&#x3c;Previous',
+		nextText: 'Next&#x3e;',
+		currentText: 'Today',
+		monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		monthNamesShort: ['Jan','Feb','Mar','Apr','May','June',
+		'Jul','Aug','Sep','Oct','Nov','Dec'],
+		dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+		dayNamesShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+		dayNamesMin: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+		weekHeader: 'Week',
+		dateFormat: 'dd.mm.yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['en']);
+});
